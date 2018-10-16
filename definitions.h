4fc1ce8941273083b27595049960271b9fb93da5
Zekromaster <dev@zekromaster.net>
2018-10-16 17:21:38 +0200

    Esercizio 3, aggiunta una licenza, separate le definizioni, documentato.

---
+++ definitions.h
@@ -0,0 +1,10 @@
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// Author
+#define AUTHOR "Zekromaster - Luca Cristiano"
+#define AUTHORMAIL "dev@zekromaster.net"
+
+// Useful stuff
+#define SUCCESS 0
