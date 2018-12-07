211f94f3f0e4b4ad6cf375530f3eb0f3044355bf
Zekromaster <dev@zekromaster.net>
2018-12-07 20:16:54 +0100

    Licensing

--- definitions.h
+++ definitions.h
@@ -1,3 +1,21 @@
+// Copyright (c) 2018 Zekromaster
+//
+// GNU GENERAL PUBLIC LICENSE
+//    Version 3, 29 June 2007
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // Libraries
 #ifndef INCLUDED_DEFINITIONS
   #define INCLUDED_DEFINITIONS
@@ -13,7 +31,6 @@
   #define VECTOR_SIZE 256
 
   // Boolean type
-  typedef int bool;
-  #define true 1
-  #define false 0
+  typedef enum {false, true} bool;
+
 #endif
